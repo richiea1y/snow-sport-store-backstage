ff7e8d2946cff0235d5879591d04e1be6a220165
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-03-02 14:10:10 +0800

    chore: add a comment to explain what is createApp and App used for.

--- src/main.js
+++ src/main.js
@@ -7,9 +7,16 @@ import { createPinia } from 'pinia';
 // import SvgIcon from '@/components/SvgIcon.vue'; // 上課時會解釋這個組件幹嘛用的
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
+/**
+ * App 通常指的是 Vue 應用程式的「根元件」（Root Component），也就是最上層的元件。
+  •	它一般定義在一個名為 App.vue 的檔案中。
+  •	所有的其他元件都會被包裹在這個根元件裡。
+ */
 import App from './App.vue';
 import router from './router';
 
+// createApp：用來建立 Vue 應用程式實例。
+// App：就是你的根元件，應用程式的起始點。
 const app = createApp(App);
 
 app.use(createPinia());
