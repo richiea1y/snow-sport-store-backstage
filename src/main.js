aa3964877c57a94c37119fd3fc8751f8cc1ca0f6
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-03-02 13:52:48 +0800

    feat(main): enhance global styles and component registration
    - Import Element Plus styles and global SVG icons.
    - Globally register Element Plus icon components.
    - Prepare structure for custom SVG icon registration (commented out).

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 import './assets/main.css'
+// import '@/styles/style.scss'; // scss入口檔案
+import 'element-plus/dist/index.css';
+import 'virtual:svg-icons-register';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+// import SvgIcon from '@/components/SvgIcon.vue'; // 上課時會解釋這個組件幹嘛用的
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from './App.vue';
+import router from './router';
 
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(router);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 
-app.use(createPinia())
-app.use(router)
+// 全局註冊 SvgIcon 組件，給自定義 icon 使用
+// app.component('SvgIcon', SvgIcon);
 
-app.mount('#app')
+app.mount('#app');
